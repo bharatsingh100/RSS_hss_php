a600b7d77300d6b99c9c9dede09ff93e1d79b9ee
Abhimanyu Gupta <abhi@hssus.org>
2010-05-24 02:41:19 +0000

    Fixing formatting of edit profile page

--- system/application/views/profile/edit-profile.php
+++ system/application/views/profile/edit-profile.php
@@ -8,19 +8,19 @@
 		     <h3>Update Information for <?=$m->first_name;?> <?=$m->last_name;?></h3>
 		     <table width="100%" border="0" cellpadding="2" cellspacing="4">
                <tr>
-                 <td width="19%"><div align="right"><strong>Name:
+                 <td width="19%"><div align="right"><strong>Name
                    </strong></div>                   <label></label></td>
                  <td colspan="3">
-                 	<input name="name" type="text" id="name" size="40" maxlength="50" 
+                 	<input name="name" type="text" id="name" size="40" maxlength="50"
                  		value="<?php echo $m->first_name,' ',$m->last_name;?>"/>
-                   &nbsp;                   
+                   &nbsp;
                    <!-- <label>
                    <input name="last_name" type="text" id="textfield2" size="30" maxlength="50" value="<?=$m->last_name;?>"/>
                    </label> -->
                    </td>
                </tr>
                <tr>
-                 <td><div align="right"><strong>Gender:</strong></div></td>
+                 <td><div align="right"><strong>Gender</strong></div></td>
                  <td colspan="3" valign="middle"><label></label>
                    <p>
                      <label>
@@ -29,7 +29,7 @@
                      <label>
                        <input type="radio" name="gender" value="F" id="RadioGroup1_1" <?php echo ($m->gender == 'F') ? 'checked="checked"' : '';?>/>
                        Sevika</label>
-                     &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<strong>Age</strong> 
+                     &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<strong>Age</strong>
                      <label>
                      <select name="birth_year" id="birth_year">
                        <option value="" <?php echo ($m->birth_year == '') ? 'selected="selected"' : '';?>></option>
@@ -50,13 +50,13 @@
                  </p></td>
                </tr>
                <tr>
-                 <td align="right"><strong>Contact Type:</strong></td>
+                 <td align="right"><strong>Contact Type</strong></td>
                  <td colspan="3" valign="middle"><select name="contact_type" id="contact_type">
                      <?php foreach($ctype as $t){ ?>
                      <option value="<?php echo $t->REF_CODE; ?>" <?php if($t->REF_CODE == $m->contact_type) echo 'selected="selected"';?>><?php echo $t->short_desc,'&nbsp;';?></option>
                      <?php } ?>
                    </select>
-                   &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<strong>Gana:</strong><select name="gana" id="gana">
+                   &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<strong>Gana</strong><select name="gana" id="gana">
                      <option>Select Gana&nbsp;</option>
 					 <?php foreach($ganas as $t){ ?>
                      <option value="<?php echo $t->REF_CODE; ?>" <?php if($t->REF_CODE == $m->gana) echo 'selected="selected"';?>><?php echo $t->short_desc,'&nbsp;';?></option>
@@ -68,16 +68,16 @@
                  <td colspan="3" valign="middle">&nbsp;</td>
                </tr>
                <tr>
-                 <td><div align="right"><strong>Company/School:</strong></div></td>
+                 <td><div align="right"><strong>Company/School</strong></div></td>
                  <td colspan="3" valign="middle"><input name="company" type="text" id="company" size="30" maxlength="50" value="<?=$m->company;?>"/>
-                   &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<strong>Position/Grade:</strong>:
+                   &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<strong>Position/Grade</strong>
                    <input name="position" type="text" id="position" size="25" maxlength="50" value="<?=$m->position;?>"/></td>
                </tr>
                <tr>
                  <td colspan="4"><h3>Contact Information</h3></td>
                </tr>
                <tr>
-                 <td><div align="right"><strong>E-mail:</strong></div></td>
+                 <td><div align="right"><strong>E-mail</strong></div></td>
                  <td colspan="3"><label>
                    <input name="email" type="text" id="email" size="35" maxlength="60" value="<?=$m->email;?>"/>
                  </label><?php if($m->email != ''): ?>
@@ -88,12 +88,17 @@
                      </select>
                      <?php endif; ?>                 </td>
                </tr>
+
                <tr>
-                 <td><div align="right"><strong>Phone:</strong></div></td>
+                 <td valign="top"><div align="right"><strong>Phone</strong></div></td>
                  <td width="9%">
-                   <input name="ph_mobile" type="text" id="ph_mobile" value="<?php echo ($m->ph_mobile == '') ? 'Mobile...' : $m->ph_mobile;?>" size="15" maxlength="12"/><span class="style2">Mobile</span></td>
-                 <td width="9%"><input name="ph_home" type="text" id="ph_home" size="15" maxlength="12" value="<?php echo ($m->ph_home == '') ? 'Home...' : $m->ph_home;?>"/>&nbsp;<span class="style2">Home</span></td>
-                 <td width="63%"><input name="ph_work" type="text" id="ph_work" maxlength="12" value="<?php echo ($m->ph_work == '') ? 'Work...' : $m->ph_work;?>" size="15" />&nbsp;<br />
+                   <input name="ph_mobile" type="text" id="ph_mobile" size="15" maxlength="14" value="<?php echo ($m->ph_mobile == '') ? 'Mobile...' : $m->ph_mobile;?>"/>
+                   <br /><span class="style2">Mobile</span></td>
+                 <td width="9%"><input name="ph_home" type="text" id="ph_home" size="15" onclick="this.value = '';" maxlength="12"
+                    value="<?php echo ($m->ph_home == '') ? 'Home...' : $m->ph_home;?>"/><br />
+                 <span class="style2">Home</span></td>
+                 <td width="63%"><input name="ph_work" type="text" id="ph_work" value="<?php echo ($m->ph_work == '') ? 'Work...' : $m->ph_work;?>" maxlength="14" size="15" />
+                 <br />
                  <span class="style2">Work</span></td>
                </tr>
                <tr>
@@ -101,7 +106,7 @@
                  <td colspan="3">&nbsp;</td>
                </tr>
                <tr>
-                 <td><div align="right"><strong>Street Address:</strong></div></td>
+                 <td><div align="right"><strong>Street Address</strong></div></td>
                  <td colspan="3"><input name="street_add1" type="text" id="street_add1" size="35" maxlength="50" value="<?=$m->street_add1;?>"/></td>
                </tr>
                <tr>
@@ -109,14 +114,14 @@
                  <td colspan="3"><input name="street_add2" type="text" id="street_add2" size="35" maxlength="50" value="<?=$m->street_add2;?>"/></td>
                </tr>
                <tr>
-                 <td><div align="right"><strong>City:</strong></div></td>
+                 <td><div align="right"><strong>City</strong></div></td>
                  <td colspan="3"><input name="city" type="text" id="city" size="35" maxlength="50" value="<?=$m->city;?>"/>                   <label></label></td>
                </tr>
                <tr>
-                 <td><div align="right"><strong>State: </strong></div></td>
+                 <td><div align="right"><strong>State </strong></div></td>
                  <td colspan="3" valign="middle"><strong>
                    <select name="state" id="select">
-                     <?php foreach($states as $var) 
+                     <?php foreach($states as $var)
 				   {
 				   		echo "<option value=\"$var->REF_CODE\"";
 						echo (($m->state == $var->REF_CODE) ? ' selected="selected"' : '');
@@ -124,7 +129,7 @@
 					}
 					?>
                      </select>
-&nbsp;&nbsp;Zip:
+&nbsp;&nbsp;Zip
 <input name="zip" type="text" id="zip" size="12" maxlength="10" value="<?=$m->zip;?>"/>
                  </strong></td>
                </tr>
@@ -137,10 +142,10 @@
                  <td colspan="4"><h3>Sangh Information</h3></td>
                </tr>
                <tr>
-                 <td align="right"><strong>Shakha:</strong></td>
+                 <td align="right"><strong>Shakha</strong></td>
                  <td colspan="3" valign="middle"><select name="shakha_id" id="shakha_id">
-                 	
-                   <?php 
+
+                   <?php
 				   $iState = $shakhas[0]->state;
 				   echo "<option>------------------$iState Shakhas------------------</option>";
 				   foreach($shakhas as $var){
@@ -156,7 +161,7 @@
                  </select>                 </td>
                </tr>
                <tr>
-                 <td><div align="right"><strong>Gatanayak:</strong></div></td>
+                 <td><div align="right"><strong>Gatanayak</strong></div></td>
                  <td colspan="3" valign="middle"><label>
                    <select name="gatanayak" id="select7">
                    <option value="" <?php echo (($m->gatanayak == '') ? 'selected="selected"' : ''); ?>></option>
@@ -169,7 +174,7 @@
                  </label></td>
                </tr>
                <tr>
-                 <td><div align="right"><strong>SSV Completed:</strong></div></td>
+                 <td><div align="right"><strong>SSV Completed</strong></div></td>
                  <td colspan="3" valign="middle"><label>
                    <select name="ssv_completed" id="select3">
                      <option value="" <?php echo ($m->ssv_completed == '') ? 'selected="selected"' : '';?>></option>
@@ -182,9 +187,9 @@
                      <option value="OTH1" <?php echo ($m->ssv_completed == 'OTH1') ? 'selected="selected"' : '';?>>Other</option>
                    </select>
                  </label></td>
-               </tr>               
+               </tr>
                <tr>
-                 <td><div align="right"><strong>Notes:</strong></div></td>
+                 <td><div align="right"><strong>Notes</strong></div></td>
                  <td colspan="3" rowspan="2" valign="top"><label>
                    <textarea name="notes" id="textarea" cols="45" rows="5"><?=$m->notes;?></textarea>
                  </label></td>
