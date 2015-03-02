b1c0644b6273ba6d3b5ffc44b439f3b0c53d8510
maheshbhanushali <bhanushali.mahesh3@gmail.com>
2015-03-02 16:51:38 +0530

    added quick contact form to MyProfile page,changed form gana & contact_type value and increased input field font size

--- system/application/config/constants.php
+++ system/application/config/constants.php
@@ -18,6 +18,9 @@ define('FILE_WRITE_MODE', 0666);
 define('DIR_READ_MODE', 0755);
 define('DIR_WRITE_MODE', 0777);
 
+define('defaultGana', '5');
+define('defaultContactType', 'RA');
+
 /*
 |--------------------------------------------------------------------------
 | File Stream Modes
