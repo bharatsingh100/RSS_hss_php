08158589bb6a5d083732ddf23b4ff6aa672ff0dc
maheshbhanushali <bhanushali.mahesh3@gmail.com>
2015-03-10 12:22:30 +0530

    removed search label

--- system/application/views/layout_search.php
+++ system/application/views/layout_search.php
@@ -47,7 +47,6 @@
 		 <!-- Begin Right Column -->
 		 <div id="rightcolumn">
          <form id="form1" name="form1" method="post" action="/search/searchRedirect">
-              <label>Search</label>
               <input type="text" name="term"
 				<?php if($this->uri->segment(1) == 'search'){
                 echo 'value="'.$this->uri->segment(4).'"';}
