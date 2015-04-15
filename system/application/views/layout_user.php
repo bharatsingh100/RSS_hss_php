951c1fe4d92476820491b3da9f3ca8c9b8b64ffd
Sysops <sysops@lifenettech.com>
2015-04-15 13:59:07 +0000

    Fix viewport header

--- system/application/views/layout_user.php
+++ system/application/views/layout_user.php
@@ -2,7 +2,7 @@
 "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-<meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0;" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1"
 />
 <title><?=$pageTitle?> - Swayamsevak Information Management - HSS</title>
